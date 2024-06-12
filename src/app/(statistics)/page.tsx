b943b68d7f44d447cd449e70c6843cb1adaf72d8
israfil-miya <israfilmiya120@gmail.com>
2024-06-13 02:26:57 +0600

    Changed default page text in statistics and formatted

--- src/app/(statistics)/page.tsx
+++ src/app/(statistics)/page.tsx
@@ -5,7 +5,11 @@ const Statistics = () => {
   return (
     <>
       <Header />
-      <p className="text-2xl">Statistics Page</p>
+      <div className="flex items-center h-[80vh]">
+        <p className="font-light text-center ml-[20%] md:ml-[40%] lg:ml-[45%]">
+          This page is under development!
+        </p>
+      </div>
     </>
   );
 };
