2ff94f2a5b2777e65342d7566ce24ba3e72c353d
israfil-miya <israfilmiya120@gmail.com>
2024-06-18 03:23:56 +0600

    Card design

--- src/app/(statistics)/components/card/Card.tsx
+++ src/app/(statistics)/components/card/Card.tsx
@@ -16,18 +16,17 @@ const Card: React.FC<CardProps> = ({
   onClick,
 }) => {
   return (
-    <div
-      className="bg-white border shadow-md rounded-lg overflow-hidden flex cursor-pointer"
-      onClick={onClick}
-    >
-      <Image
-        width={100}
-        height={100}
-        src={imageUrl}
-        alt={title}
-      />
-
-      <div className="p-4">
+    <div className="bg-white shadow-md overflow-hidden flex cursor-pointer w-auto border">
+      <div className="w-1/3">
+        <Image
+          src={imageUrl}
+          alt={title}
+          width={50} // Set a suitable width
+          height={50} // Set a suitable height
+          className="w-full h-full object-cover"
+        />
+      </div>
+      <div className="w-2/3 p-4">
         <h2 className="text-xl font-bold mb-2">{title}</h2>
         <p className="text-gray-700">{description}</p>
       </div>
--- src/app/(statistics)/components/card/Cards.tsx
+++ src/app/(statistics)/components/card/Cards.tsx
@@ -6,7 +6,7 @@ import Card from './Card';
 
 const Cards = () => {
   return (
-    <div className="inline-flex gap-3 my-4">
+    <div className="grid grid-cols-2 gap-4 my-4">
       <Card
         title="Trial Clients"
         imageUrl="https://images.unsplash.com/photo-1675497219360-270db3f96e06?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
